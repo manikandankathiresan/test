986dc0b1a484ab166d154415cc6e64fbec587f65
Vishva <vishva.s@mitrahsoft.com>
2023-07-14 13:02:00 +0530

    map function added

--- vishva_functions.js
+++ vishva_functions.js
@@ -1,28 +1,7 @@
-//factorial function
-const factorial=(num)=>{
-    let fact=1
-    for(let i=1;i<=num;i++){
-        return fact=fact*1
-    }
-}
-factorial(4)
+//map function
 
-//multiply function
-const multiply = () => 5 * 11 
-multiply()
+const array1 = [1, 4, 9, 16];
+const map1 = array1.map(x => x * 2);
+console.log(map1);
 
-//divide function
-const divide = () => 77/7
-divide()
-
-//squareNumber function
-const squareNumber = () => 3*3 
-squareNumber()
-
-//inbuild function
-const inBuildFunctions=()=>{
-    console.log(number.toString())
-    console.log(string.toUpperCase())
-}
-inBuildFunctions()
-console.log("Vishwa functions");
+//output : [ 2, 8, 18, 32 ]
