3c0c08ebcce66d0fc7a362b2573428491788ea6e
boopesh <boopesh.t@mithrahsoft.com>
2023-07-14 12:43:11 +0530

    file changed to js

---
+++ task.js
@@ -0,0 +1,30 @@
+//Multiplication
+const multiply = (a, b) => {
+  return a * b;
+};
+console.log(multiply(10,20))
+
+//greetFunction
+const greetFunction = () => {
+    return "Good evening"
+};
+console.log(greetFunction());
+
+// name function
+const data = () => {
+  let name = "sathish";
+  return name;
+};
+console.log(data());
+
+//subtraction
+const subtraction = (a, b) => {
+  return a - b;
+};
+console.log(subtraction(20, 10));
+
+// github
+const github = () =>{
+return 5+6
+}
+console.log(github());
