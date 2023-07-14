f132cbf0097f095e03c846e2236354115b696ec5
Vishva <vishva.s@mitrahsoft.com>
2023-07-14 12:47:56 +0530

    Text file is converted to js file format

---
+++ vishva_functions.js
@@ -0,0 +1,28 @@
+//factorial function
+const factorial=(num)=>{
+    let fact=1
+    for(let i=1;i<=num;i++){
+        return fact=fact*1
+    }
+}
+factorial(4)
+
+//multiply function
+const multiply = () => 5 * 11 
+multiply()
+
+//divide function
+const divide = () => 77/7
+divide()
+
+//squareNumber function
+const squareNumber = () => 3*3 
+squareNumber()
+
+//inbuild function
+const inBuildFunctions=()=>{
+    console.log(number.toString())
+    console.log(string.toUpperCase())
+}
+inBuildFunctions()
+console.log("Vishwa functions");
