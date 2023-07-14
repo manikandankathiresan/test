c8e7d053e1819f3eb46843443d63da202fea694e
boopesh <boopesh.t@mithrahsoft.com>
2023-07-14 13:22:06 +0530

    object function added

--- vishva_functions.js
+++ vishva_functions.js
@@ -5,3 +5,15 @@ const map1 = array1.map(x => x * 2);
 console.log(map1);
 
 //output : [ 2, 8, 18, 32 ]
+
+
+//boopesh object entries
+
+const object1 = {
+    a: 'somestring',
+    b: 42
+  };
+  
+  for (const [key, value] of Object.entries(object1)) {
+    console.log(`${key}: ${value}`);
+  }
